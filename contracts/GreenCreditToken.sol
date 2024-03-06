04cdf261aeef0345588324d0da8dd43528709f14
Parth_Nath_Chauhan <parth26nath@gmail.com>
2024-03-06 08:59:55 +0530

    Update GreenCreditToken.sol

--- contracts/GreenCreditToken.sol
+++ contracts/GreenCreditToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract EcoCreditToken is ERC20 {
+contract GreenCreditToken is ERC20 {
     address payable public EcoChainPlatformOwner;
     uint public tokenPrice;
 
